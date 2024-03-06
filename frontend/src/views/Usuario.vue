a1b3cc0bd71990796a5bf7e7713445fd64d0442e
richwrd <dudurichard07@gmail.com>
2024-03-06 00:02:53 -0300

    front

---
+++ frontend/src/views/Usuario.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- HTML -->
+</template>
+
+<script>
+export default {
+    export default {
+        name: 'NavBar',
+        props: {
+            msg: String
+        }
+    }
+}
+</script>
+
+<style>
+    /* css */
+</style>
